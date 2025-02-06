5eeef17d69cf7b5fccf57073c923b665ec1aa32a
mark636 <mark.mathew@students.mq.edu.au>
2025-02-06 16:36:15 +1100

    updated form2 dataviewgrid removed from form

--- Test/Form2.Designer.vb
+++ Test/Form2.Designer.vb
@@ -23,13 +23,8 @@ Partial Class Form2
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.txtDistanceToFinish = New System.Windows.Forms.TextBox()
-        Me.dgvPowerRanges = New System.Windows.Forms.DataGridView()
-        Me.Range_Start = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.Range_End = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.Target_Power = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.LbSpeed = New System.Windows.Forms.Label()
         Me.btnSave = New System.Windows.Forms.Button()
-        CType(Me.dgvPowerRanges, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'txtDistanceToFinish
@@ -37,35 +32,12 @@ Partial Class Form2
         Me.txtDistanceToFinish.BackColor = System.Drawing.Color.White
         Me.txtDistanceToFinish.Font = New System.Drawing.Font("Nirmala UI", 15.0!, System.Drawing.FontStyle.Bold)
         Me.txtDistanceToFinish.ForeColor = System.Drawing.Color.Black
-        Me.txtDistanceToFinish.Location = New System.Drawing.Point(160, 27)
+        Me.txtDistanceToFinish.Location = New System.Drawing.Point(213, 33)
+        Me.txtDistanceToFinish.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.txtDistanceToFinish.Multiline = True
         Me.txtDistanceToFinish.Name = "txtDistanceToFinish"
-        Me.txtDistanceToFinish.Size = New System.Drawing.Size(121, 34)
+        Me.txtDistanceToFinish.Size = New System.Drawing.Size(160, 41)
         Me.txtDistanceToFinish.TabIndex = 10
-        '
-        'dgvPowerRanges
-        '
-        Me.dgvPowerRanges.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dgvPowerRanges.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Range_Start, Me.Range_End, Me.Target_Power})
-        Me.dgvPowerRanges.Location = New System.Drawing.Point(12, 79)
-        Me.dgvPowerRanges.Name = "dgvPowerRanges"
-        Me.dgvPowerRanges.Size = New System.Drawing.Size(343, 150)
-        Me.dgvPowerRanges.TabIndex = 11
-        '
-        'Range_Start
-        '
-        Me.Range_Start.HeaderText = "Range_Start"
-        Me.Range_Start.Name = "Range_Start"
-        '
-        'Range_End
-        '
-        Me.Range_End.HeaderText = "Range_End"
-        Me.Range_End.Name = "Range_End"
-        '
-        'Target_Power
-        '
-        Me.Target_Power.HeaderText = "Target_Power"
-        Me.Target_Power.Name = "Target_Power"
         '
         'LbSpeed
         '
@@ -73,9 +45,10 @@ Partial Class Form2
         Me.LbSpeed.BackColor = System.Drawing.Color.Transparent
         Me.LbSpeed.Font = New System.Drawing.Font("Nirmala UI", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.LbSpeed.ForeColor = System.Drawing.Color.Transparent
-        Me.LbSpeed.Location = New System.Drawing.Point(12, 27)
+        Me.LbSpeed.Location = New System.Drawing.Point(16, 33)
+        Me.LbSpeed.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.LbSpeed.Name = "LbSpeed"
-        Me.LbSpeed.Size = New System.Drawing.Size(152, 28)
+        Me.LbSpeed.Size = New System.Drawing.Size(191, 35)
         Me.LbSpeed.TabIndex = 78
         Me.LbSpeed.Text = "SET DISTANCE:"
         '
@@ -86,35 +59,31 @@ Partial Class Form2
         Me.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.btnSave.Font = New System.Drawing.Font("Nirmala UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnSave.ForeColor = System.Drawing.Color.Transparent
-        Me.btnSave.Location = New System.Drawing.Point(17, 251)
+        Me.btnSave.Location = New System.Drawing.Point(22, 113)
+        Me.btnSave.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.btnSave.Name = "btnSave"
-        Me.btnSave.Size = New System.Drawing.Size(337, 32)
+        Me.btnSave.Size = New System.Drawing.Size(449, 39)
         Me.btnSave.TabIndex = 79
         Me.btnSave.Text = "Save"
         Me.btnSave.UseVisualStyleBackColor = True
         '
         'Form2
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(46, Byte), Integer), CType(CType(51, Byte), Integer), CType(CType(73, Byte), Integer))
-        Me.ClientSize = New System.Drawing.Size(366, 311)
+        Me.ClientSize = New System.Drawing.Size(488, 176)
         Me.Controls.Add(Me.btnSave)
         Me.Controls.Add(Me.LbSpeed)
-        Me.Controls.Add(Me.dgvPowerRanges)
         Me.Controls.Add(Me.txtDistanceToFinish)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "Form2"
         Me.Text = "Settings"
-        CType(Me.dgvPowerRanges, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
     Friend WithEvents txtDistanceToFinish As TextBox
-    Friend WithEvents dgvPowerRanges As DataGridView
-    Friend WithEvents Range_Start As DataGridViewTextBoxColumn
-    Friend WithEvents Range_End As DataGridViewTextBoxColumn
-    Friend WithEvents Target_Power As DataGridViewTextBoxColumn
     Friend WithEvents LbSpeed As Label
     Friend WithEvents btnSave As Button
 End Class
--- Test/Form2.vb
+++ Test/Form2.vb
@@ -1,63 +1,10 @@
 ﻿Public Class Form2
     ' Define properties to store and retrieve settings
     Public Property DistanceToFinish As Decimal
-    Public Property TargetPowerRanges As New List(Of (Decimal, Decimal, Integer))
-
     Private Sub SettingsForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         ' Load Distance to Finish
         txtDistanceToFinish.Text = DistanceToFinish.ToString()
 
-        ' Initialize the DataGridView
-        SetupDataGridView()
-
-        ' Populate the DataGridView with existing ranges
-        For Each range In TargetPowerRanges
-            dgvPowerRanges.Rows.Add(range.Item1, range.Item2, range.Item3)
-        Next
-    End Sub
-
-    Private Sub dgvPowerRanges_CellValidating(sender As Object, e As DataGridViewCellValidatingEventArgs) Handles dgvPowerRanges.CellValidating
-        ' Validate numeric inputs
-        If e.ColumnIndex = dgvPowerRanges.Columns("RangeStart").Index OrElse
-           e.ColumnIndex = dgvPowerRanges.Columns("RangeEnd").Index Then
-            Dim value As Decimal
-            If Not Decimal.TryParse(e.FormattedValue?.ToString(), value) Then
-                MessageBox.Show("Please enter a valid decimal number.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Error)
-                e.Cancel = True
-            End If
-        ElseIf e.ColumnIndex = dgvPowerRanges.Columns("TargetPower").Index Then
-            Dim value As Integer
-            If Not Integer.TryParse(e.FormattedValue?.ToString(), value) Then
-                MessageBox.Show("Please enter a valid integer.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Error)
-                e.Cancel = True
-            End If
-        End If
-    End Sub
-
-    Private Sub SetupDataGridView()
-        ' Clear existing setup
-        dgvPowerRanges.Columns.Clear()
-
-        ' Add Range Start column
-        Dim colRangeStart As New DataGridViewTextBoxColumn()
-        colRangeStart.Name = "RangeStart"
-        colRangeStart.HeaderText = "Range Start"
-        colRangeStart.ValueType = GetType(Decimal)
-        dgvPowerRanges.Columns.Add(colRangeStart)
-
-        ' Add Range End column
-        Dim colRangeEnd As New DataGridViewTextBoxColumn()
-        colRangeEnd.Name = "RangeEnd"
-        colRangeEnd.HeaderText = "Range End"
-        colRangeEnd.ValueType = GetType(Decimal)
-        dgvPowerRanges.Columns.Add(colRangeEnd)
-
-        ' Add Target Power column
-        Dim colTargetPower As New DataGridViewTextBoxColumn()
-        colTargetPower.Name = "TargetPower"
-        colTargetPower.HeaderText = "Target Power (W)"
-        colTargetPower.ValueType = GetType(Integer)
-        dgvPowerRanges.Columns.Add(colTargetPower)
     End Sub
 
     Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
@@ -67,32 +14,6 @@
                 MessageBox.Show("Invalid Distance to Finish value.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
                 Return
             End If
-
-            ' Validate and save Power Ranges
-            TargetPowerRanges.Clear()
-            For Each row As DataGridViewRow In dgvPowerRanges.Rows
-                If row.IsNewRow Then Continue For ' Skip the new row placeholder
-
-                ' Validate inputs
-                Dim rangeStart As Decimal
-                Dim rangeEnd As Decimal
-                Dim targetPower As Integer
-
-                If Not Decimal.TryParse(row.Cells("RangeStart").Value?.ToString(), rangeStart) OrElse
-                   Not Decimal.TryParse(row.Cells("RangeEnd").Value?.ToString(), rangeEnd) OrElse
-                   Not Integer.TryParse(row.Cells("TargetPower").Value?.ToString(), targetPower) Then
-                    MessageBox.Show("Invalid input in power ranges.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
-                    Return
-                End If
-
-                If rangeStart >= rangeEnd Then
-                    MessageBox.Show("Range Start must be less than Range End.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
-                    Return
-                End If
-
-                ' Add to list
-                TargetPowerRanges.Add((rangeStart, rangeEnd, targetPower))
-            Next
 
             ' Save successful
             Me.DialogResult = DialogResult.OK
